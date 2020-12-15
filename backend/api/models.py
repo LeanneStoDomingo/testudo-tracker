225df0ce7367f0a7c5d1184431376bdc9a00c2e9
Leanne Sto. Domingo <leanneasd@gmail.com>
2020-12-14 20:49:33 -0500

    Configured django models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Department(models.Model):
+    code = models.CharField(max_length=4)
+    name = models.CharField(max_length=50)
+
+class Course(models.Model):
+    code = models.CharField(max_length=8)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    department_id = models.ForeignKey(Department, on_delete=models.PROTECT)
+
+class Section(models.Model):
+    code = models.CharField(max_length=4)
+    course_id = models.ForeignKey(Course, on_delete=models.PROTECT)
+
+class Professor(models.Model):
+    name = models.CharField(max_length=50)
+
+class Day(models.Model):
+    day = models.IntegerField()
+    semester = models.IntegerField()
+    total_seats = models.IntegerField()
+    open_seats = models.IntegerField()
+    waitlist_seats = models.IntegerField()
+    holdfile_seats = models.IntegerField()
+    professor_id = models.ForeignKey(Professor, on_delete=models.PROTECT)
+    section_id = models.ForeignKey(Section, on_delete=models.PROTECT)
