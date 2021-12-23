a178e8f5b4c485aacf3c16cf28c9717089fc3018
Leanne Sto. Domingo <leanneasd@gmail.com>
2021-12-23 18:37:05 -0500

    Start styling home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,13 +2,25 @@ import Head from 'next/head'
 
 const Home = () => {
     return (
-        <div>
+        <>
             <Head>
                 <title>Testudo Tracker</title>
                 <meta name='description' content='A student run website that tracks seat availability for courses at the University of Maryland, College Park' />
             </Head>
-            Testudo Tracker
-        </div>
+            <div className='flex flex-col text-center'>
+                <h1 className='text-6xl font-semibold'>Testudo Tracker</h1>
+                <p className='text-xl'>A student run website that tracks seat availability for courses at the University of Maryland, College Park</p>
+                <div className='flex justify-center gap-3 mx-2 my-5 text-xl'>
+                    <input className='w-3/4 max-w-2xl rounded p-3 text-black focus:outline-none focus:ring focus:ring-primary-700 focus:ring-offset-4 focus:ring-offset-primary-500' placeholder='Search for Courses, Professors, Departments, GenEds, etc...' />
+                    <button className='bg-primary-700 rounded py-3 px-4 focus:outline-none focus:ring focus:ring-primary-700 focus:ring-offset-4 focus:ring-offset-primary-500'>Search</button>
+                </div>
+            </div>
+            <div className='bg-white text-black'>
+                <h2 className='text-3xl text-center'>Popular Courses</h2>
+                <div></div>
+            </div>
+            <div></div>
+        </>
     )
 }
 
