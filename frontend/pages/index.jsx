041c2d635b49515fba2b91557475b309aea711ea
Leanne Sto. Domingo <leanneasd@gmail.com>
2021-12-26 12:17:52 -0500

    Add navigation to search page

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,10 +1,25 @@
 import useSearch from '@utils/useSearch'
 import Head from 'next/head'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 
 const Home = () => {
     const [query, setQuery] = useState('')
     const { search, loading, error } = useSearch()
+    const router = useRouter()
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        const filtered = search.filter((item) => item.text.toLowerCase().includes(query.toLowerCase()))
+
+        if (filtered.length === 1) {
+            router.push(filtered[0].link)
+        } else {
+            router.push(`/search?query=${query}`)
+        }
+    }
 
     return (
         <>
@@ -15,7 +30,7 @@ const Home = () => {
             <div className='flex flex-col text-center mx-2'>
                 <h1 className='text-5xl sm:text-6xl font-semibold'>Testudo Tracker</h1>
                 <p className='text-xl'>A student run website that tracks seat availability for courses at the University of Maryland, College Park</p>
-                <div className='flex justify-center gap-3 my-5 text-xl '>
+                <form className='flex justify-center gap-3 my-5 text-xl' onSubmit={onSubmit}>
                     <div className='flex-col w-3/4 relative max-w-2xl'>
                         <input onChange={(e) => setQuery(e.target.value)} value={query} className='w-full rounded p-3 text-black focus:outline-none focus:ring-4 focus:ring-primary-900/50' placeholder='Search for Courses, Professors, Departments, GenEds, etc...' />
                         <div className='absolute bg-white text-black shadow-lg rounded mt-1 w-full wrap text-base md:text-xl text-left'>
@@ -24,12 +39,12 @@ const Home = () => {
                                     search
                                         .filter((item) => item.text.toLowerCase().includes(query.toLowerCase()))
                                         .slice(0, 10)
-                                        .map((item) => <li key={item.link} className='rounded p-2 hover:bg-primary-50'>{item.text}</li>)}
+                                        .map((item) => <li key={item.link} className='rounded p-2 hover:bg-primary-50 cursor-pointer'><Link href={item.link}>{item.text}</Link></li>)}
                             </ul>
                         </div>
                     </div>
-                    <button className='bg-primary-700 rounded py-3 px-4 focus:outline-none focus:ring-4 focus:ring-primary-900/[.70] hover:bg-primary-800'>Search</button>
-                </div>
+                    <input className='bg-primary-700 rounded py-3 px-4 focus:outline-none focus:ring-4 focus:ring-primary-900/[.70] hover:bg-primary-800' type='submit' value='Submit' />
+                </form>
             </div>
             <div className='bg-white text-black'>
                 <h2 className='text-3xl text-center'>Popular Courses</h2>
--- frontend/pages/search.jsx
+++ frontend/pages/search.jsx
@@ -1,7 +1,48 @@
+import useSearch from "@utils/useSearch"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+
 const Search = () => {
+    const router = useRouter()
+    const [query, setQuery] = useState('')
+    const { search, loading, error } = useSearch()
+
+    useEffect(() => {
+        setQuery(router.query.query || '')
+    }, [router.query.query])
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        const filtered = search.filter((item) => item.text.toLowerCase().includes(query.toLowerCase()))
+
+        if (filtered.length === 1) {
+            router.push(filtered[0].link)
+        } else {
+            router.push(`/search?query=${query}`)
+        }
+    }
+
     return (
-        <div>
-            search
+        <div className="bg-white text-black">
+            <h1>Search</h1>
+            <form className='flex justify-center gap-3 my-5 text-xl pb-10' onSubmit={onSubmit}>
+                <div className='flex-col w-3/4 relative max-w-2xl'>
+                    <input onChange={(e) => setQuery(e.target.value)} value={query} className='peer shadow-2xl w-full rounded p-3 text-black focus:outline-none focus:ring-4 focus:ring-primary-900/50' placeholder='Search for Courses, Professors, Departments, GenEds, etc...' />
+                    <div className='hidden focus-within:block peer-focus:block absolute bg-white text-black shadow-lg rounded mt-1 w-full wrap text-base md:text-xl text-left'>
+                        <ul>
+                            {query?.length != 0 && !error && !loading &&
+                                search
+                                    .filter((item) => item.text.toLowerCase().includes(query?.toLowerCase()))
+                                    .slice(0, 10)
+                                    .map((item) => <li key={item.link} className='rounded p-2 hover:bg-primary-50 cursor-pointer'><Link href={item.link}>{item.text}</Link></li>)}
+                        </ul>
+                    </div>
+                </div>
+                <input className='shadow-2xl bg-primary-700 rounded py-3 px-4 focus:outline-none focus:ring-4 focus:ring-primary-900/[.70] hover:bg-primary-800 text-white' type='submit' value='Submit' />
+            </form>
+            <div></div>
         </div>
     )
 }
