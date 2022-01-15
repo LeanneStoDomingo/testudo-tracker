b9d3759bcbbbb49ce6e1960431ae9aefd1cab182
Leanne Sto. Domingo <leanneasd@gmail.com>
2022-01-15 17:55:35 -0500

    Add chart component

---
+++ frontend/components/Chart.jsx
@@ -0,0 +1,26 @@
+import { Line } from "react-chartjs-2"
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js'
+
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+const Chart = ({ data }) => <Line data={data} />
+
+export default Chart
--- frontend/pages/courses/[code].jsx
+++ frontend/pages/courses/[code].jsx
@@ -1,11 +1,43 @@
-const Course = () => {
+import Chart from "@components/Chart"
+import axios from "@utils/axios"
+import formatChartData from "@utils/formatChartData"
+import { useRouter } from "next/router"
+
+const Course = ({ name, data }) => {
+    const router = useRouter()
+    const { code } = router.query
+
     return (
         <>
-            <h1>code</h1>
-            <p>name</p>
-            <div>chart</div>
+            <h1>{code}</h1>
+            <p>{name}</p>
+            <Chart data={data} />
         </>
     )
 }
 
 export default Course
+
+export const getStaticPaths = async () => {
+    const { data } = await axios.get('/search')
+    const courses = data.data.filter((item) => item.link.includes('/courses/'))
+
+    // const paths = courses.map((course) => ({ params: { code: course.code } }))
+    const paths = courses.map((course) => ({ params: { code: course.id.toString() } }))
+
+    return {
+        paths,
+        fallback: 'blocking'
+    }
+}
+
+export const getStaticProps = async ({ params: { code } }) => {
+    const { data: { name, seats } } = await axios.get(`/courses/${code}`)
+
+    return {
+        props: {
+            name,
+            data: formatChartData(seats)
+        }
+    }
+}
