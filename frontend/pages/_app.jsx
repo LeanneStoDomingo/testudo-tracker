fdbb11384e8fb811124c28aef3b6c2502f2c9f13
Leanne Sto. Domingo <leanneasd@gmail.com>
2022-01-15 14:20:25 -0500

    Move font import

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -12,6 +12,9 @@ const App = ({ Component, pageProps }) => {
                 <title>Testudo Tracker</title>
                 <meta name='description' content='A student run website that tracks seat availability for courses at the University of Maryland, College Park' />
                 <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
             </Head>
             <SWRConfig value={{ fetcher }}>
                 <Header />
