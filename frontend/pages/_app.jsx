22290cd438e8acc73c82a62cb34d19d92e5f0333
Leanne Sto. Domingo <leanneasd@gmail.com>
2022-01-19 22:06:08 -0500

    Fix crossOrigin error

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -13,7 +13,7 @@ const App = ({ Component, pageProps }) => {
                 <meta name='description' content='A student run website that tracks seat availability for courses at the University of Maryland, College Park' />
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
                 <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
             </Head>
             <SWRConfig value={{ fetcher }}>
