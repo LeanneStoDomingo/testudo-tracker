5507baebb97991bd74565af8199779c629ede0fa
Leanne Sto. Domingo <leanneasd@gmail.com>
2022-01-19 23:13:38 -0500

    Reformat redirect function

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-    redirects: async () => (
-        [
-            {
-                source: '/github',
-                destination: 'https://github.com/LeanneStoDomingo/testudo-tracker',
-                permanent: true,
-            },
-            {
-                source: '/reddit',
-                destination: 'https://www.reddit.com/user/testudo-tracker',
-                permanent: true,
-            },
-        ]
-    )
+    redirects: async () => [
+        {
+            source: '/github',
+            destination: 'https://github.com/LeanneStoDomingo/testudo-tracker',
+            permanent: true,
+        },
+        {
+            source: '/reddit',
+            destination: 'https://www.reddit.com/user/testudo-tracker',
+            permanent: true,
+        },
+    ]
 }
