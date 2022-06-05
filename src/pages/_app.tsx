d51a9101c75b76b2fdd2db5ae3b944f282c86ada
Leanne Sto. Domingo <leanneasd@gmail.com>
2022-06-05 02:33:41 -0400

    Add tailwindcss and configure prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import "@/styles/globals.css";
+import "@fontsource/inter";
 import type { AppProps } from "next/app";
 
 function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+  return <Component {...pageProps} />;
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
