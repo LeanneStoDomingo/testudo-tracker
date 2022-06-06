4d04aee297a57e9f4578fedd3fff6cf19cb2134e
Leanne Sto. Domingo <leanneasd@gmail.com>
2022-06-05 22:00:09 -0400

    Add umami analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          strategy="afterInteractive"
+          data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID}
+          src={process.env.NEXT_PUBLIC_UMAMI_URL}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
