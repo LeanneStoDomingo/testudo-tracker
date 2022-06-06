756e658a6f3cbb965a14022a3707f5531f8acaab
Leanne Sto. Domingo <leanneasd@gmail.com>
2022-06-05 22:19:36 -0400

    Fix umami script placement

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import "@/styles/globals.css";
 import "@fontsource/inter";
 import Footer from "@/components/Footer";
@@ -7,6 +8,11 @@ import Header from "@/components/Header";
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID}
+        src={process.env.NEXT_PUBLIC_UMAMI_URL}
+      />
       <Header />
       <main>
         <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <Script
-          strategy="afterInteractive"
-          data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID}
-          src={process.env.NEXT_PUBLIC_UMAMI_URL}
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
