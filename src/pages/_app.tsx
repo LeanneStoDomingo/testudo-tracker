0e41b0945bc5496ec169764de19a66f838376d70
Leanne Sto. Domingo <leanneasd@gmail.com>
2023-03-25 22:01:49 -0400

    Start home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import { type AppType } from "next/app";
+import { Inter } from "next/font/google";
+import Head from "next/head";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
@@ -6,14 +8,40 @@ import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --font-sans: ${inter.style.fontFamily};
+          }
+        `}
+      </style>
+      <Head>
+        <title>Testudo Tracker</title>
+        <meta
+          name="description"
+          content="A student run website that tracks seat availability for courses at the University of Maryland, College Park"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SessionProvider session={session}>
+        <header>Header</header>
+        <main>
+          <Component {...pageProps} />
+        </main>
+        <footer>Footer</footer>
+      </SessionProvider>
+    </>
   );
 };
 
