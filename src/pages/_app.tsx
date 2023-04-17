eb5f082b4ca304fcdc2a9dedc450236f733c6d06
Leanne Sto. Domingo <leanneasd@gmail.com>
2023-04-17 13:22:10 -0400

    Add toast to onError

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import { type AppType } from "next/app";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import { TRPCClientError, httpBatchLink, loggerLink } from "@trpc/client";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import superjson from "superjson";
 
 import { api } from "@/utils/api";
+import { useToast } from "@/hooks/use-toast";
+import { Toaster } from "@/components/ui/toaster";
 
 import "@/styles/globals.css";
 
@@ -13,10 +23,68 @@ const inter = Inter({
   variable: "--font-sans",
 });
 
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const { toast } = useToast();
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        queryCache: new QueryCache({
+          onError: (err, query) => {
+            let title = "Oops! Something went wrong";
+            let description = [""];
+
+            if (err instanceof TRPCClientError) {
+              title = "Error: " + err.message;
+              description = [
+                "Endpoint: " + (query.queryKey[0] as string[]).join("."),
+                "Input: " +
+                  JSON.stringify(
+                    (query.queryKey[1] as { input: unknown }).input
+                  ),
+              ];
+            }
+
+            toast({
+              variant: "destructive",
+              title,
+              description: description.map((line, i) => <p key={i}>{line}</p>),
+            });
+          },
+        }),
+      })
+  );
+
+  const [trpcClient] = useState(() =>
+    api.createClient({
+      transformer: superjson,
+
+      /**
+       * Links used to determine request flow from client to server.
+       *
+       * @see https://trpc.io/docs/links
+       */
+      links: [
+        loggerLink({
+          enabled: (opts) =>
+            process.env.NODE_ENV === "development" ||
+            (opts.direction === "down" && opts.result instanceof Error),
+        }),
+        httpBatchLink({
+          url: `${getBaseUrl()}/api/trpc`,
+        }),
+      ],
+    })
+  );
+
   return (
     <>
       <style jsx global>
@@ -34,15 +102,20 @@ const MyApp: AppType<{ session: Session | null }> = ({
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <SessionProvider session={session}>
-        <header>Header</header>
-        <main>
-          <Component {...pageProps} />
-        </main>
-        <footer>Footer</footer>
-      </SessionProvider>
+      <api.Provider client={trpcClient} queryClient={queryClient}>
+        <QueryClientProvider client={queryClient}>
+          <SessionProvider session={session}>
+            <header>Header</header>
+            <main>
+              <Component {...pageProps} />
+            </main>
+            <footer>Footer</footer>
+            <Toaster />
+          </SessionProvider>
+        </QueryClientProvider>
+      </api.Provider>
     </>
   );
 };
 
-export default api.withTRPC(MyApp);
+export default MyApp;
