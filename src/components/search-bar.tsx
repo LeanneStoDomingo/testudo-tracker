9f31e9f02756dbf80b4b675654093175c43abce8
Leanne Sto. Domingo <leanneasd@gmail.com>
2023-04-17 21:11:35 -0400

    Refactor search bar

---
+++ src/components/search-bar.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { z } from "zod";
+
+import { api } from "@/utils/api";
+import { useZodForm } from "@/hooks/use-zod-form";
+import { useDebounce } from "@/hooks/use-debounce";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Spinner } from "@/components/ui/spinner";
+
+const schema = z.object({
+  query: z.string().min(1),
+});
+
+const SearchBar: React.FC<{ defaultQuery?: string }> = ({
+  defaultQuery = "",
+}) => {
+  const router = useRouter();
+
+  const form = useZodForm({ schema, defaultValues: { query: defaultQuery } });
+  const query = form.watch("query").trim();
+
+  const debouncedQuery = useDebounce(query);
+
+  const search = api.search.useQuery(
+    { query: debouncedQuery },
+    { enabled: !!debouncedQuery, keepPreviousData: true }
+  );
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) =>
+    void form.handleSubmit((data) => {
+      const link =
+        search.data?.length === 1 && !!search.data[0]?.link
+          ? search.data[0].link
+          : `/search?query=${data.query}`;
+
+      void router.push(link);
+    })(e);
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <Input {...form.register("query")} />
+        <Button>Search</Button>
+        {search.isLoading && !!query && <Spinner />}
+      </form>
+      {!!query && (
+        <ul>
+          {search.data?.map((item) => (
+            <li key={item.link}>
+              <Link href={item.link}>{item.label}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default SearchBar;
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,16 +1,28 @@
-import { type NextPage } from "next";
-import { useRouter } from "next/router";
+import { type GetServerSideProps, type NextPage } from "next";
 
-const Search: NextPage = () => {
-  const router = useRouter();
-  const { query } = router.query;
+import SearchBar from "@/components/search-bar";
 
+const Search: NextPage<{ query: string }> = ({ query }) => {
   return (
     <>
       <h1>Search</h1>
       <p>Query: {query}</p>
+      <SearchBar defaultQuery={query} />
     </>
   );
+};
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const query = context.query.query;
+
+  if (typeof query !== "string") throw new Error("Invalid query");
+
+  return {
+    props: {
+      query,
+    },
+  };
 };
 
 export default Search;
